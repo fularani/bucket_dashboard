060fa07b146e989df54ef76725eb9d79e50cfb49
fularani <ranivhansure@gmail.com>
2022-08-29 14:14:45 +0530

    made one root component

--- webui/src/Components/HeaderSection.js
+++ webui/src/Components/HeaderSection.js
@@ -24,23 +24,29 @@ const HeaderSection = () => {
             padding: 0, color: '#ffffff', backgroundColor: '#081F2C', height: '15vh !important',
         }}>
             <div className="d-flex justify-content-between">
-                <Pageheader pagehead={location.state ? location.state.pagehead : 'ZENKO ORBIT'} pagetitle={location.state ? location.state.pagetitle : 'Dashboard'} />
-                <Affix offsetTop={top} className="d-flex align-items-start">
-                    <Button size="small" type="text" className="text-white w-auto h-auto" onClick={() => setTop(top + 10)} >
-                        <Avatar
-                            size="small" style={{
-                                color: '#081F2C',
-                                backgroundColor: '#ffffff',
-                            }}
-                        >U</Avatar>
-                        <Text className="p-2 text-white">Registered User Name</Text>
-                    </Button>
-                    <span className="text-white">|</span>
-                    <Button size="small" type="text" className="text-white h-auto" onClick={() => { }} >
-                        <HiOutlineChatAlt2 size={20} className="m-1" />
-                        <Text className="text-white">Get Help on Zenko Forum</Text>
-                    </Button>
-                </Affix>
+                <div>
+                    <Pageheader pagehead={location.state ? location.state.pagehead : 'ZENKO ORBIT'} pagetitle={location.state ? location.state.pagetitle : 'Dashboard'} />
+                </div>
+                <div>
+                    <Affix offsetTop={top} className="d-flex align-items-start">
+                        <div>
+                            <Button size="small" type="text" className="text-white w-auto h-auto" onClick={() => setTop(top + 10)} >
+                                <Avatar
+                                    size="small" style={{
+                                        color: '#081F2C',
+                                        backgroundColor: '#ffffff',
+                                    }}
+                                >U</Avatar>
+                                <Text className="p-2 text-white">Registered User Name</Text>
+                            </Button>
+                            <span className="text-white">|</span>
+                            <Button size="small" type="text" className="text-white h-auto" onClick={() => { }} >
+                                <HiOutlineChatAlt2 size={20} className="m-1" />
+                                <Text className="text-white">Get Help on Zenko Forum</Text>
+                            </Button>
+                        </div>
+                    </Affix>
+                </div>
             </div>
         </Header>
     )
